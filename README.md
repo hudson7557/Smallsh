9e62a23ee66fa87eb01443ea52119d9088f66932
Scott Hudson <hudsonsc@oregonstate.edu>
2020-10-24 11:10:03 -0700

    Reaind in the arguments is relatively working right now.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hudsonsc_program3
 CS 344 Program 3
+
+The program will exit only when "exit" is entered. If you enter "exit " it will not exit as it should. 
--- program3.c
+++ program3.c
@@ -11,59 +11,24 @@ struct parameters
     struct parameters *next;
 };
 
-int processCommand(char *userCmd, char *ptr)
+void printArgs(char *args, int i)
 {
-    char *argument;
-    char *token;
-
-    while (strcmp(argument, "\n") != 0)
+    for (int j=0; j < i; j++)
     {
-        token = strtok_r(NULL, " ", &ptr);
-        argument = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(argument, token);
-
-        printf("%s", argument);
-    };
-
-    return 0;
-}
-/* 
-* Parse the userCommand which is space delimited and create a parameters struct with the data in the line
-* Adapted from <CS 344> (<10/12/20>) <main.c> [<sample program>]. https://repl.it/@cs344/studentsc#main.c.
-* to create a semi efficient list to handle parameters and allow for checking for $$.
-
-struct parameters processCommand(char *userCmd)
-{
-    struct parameters *currParameter = malloc(sizeof(struct parameters));
-
-    // For use with strtok_r
-    char *savePtr;
-    int ampersandPresent = 1; 
-
-    // The first token is the command 
-    char *token = strtok_r(userCmd, " ", &saveptr);
-    char command = calloc(strlen(token) + 1, sizeof(char));
-    strcpy(command, token);
-
-    printf("%s\n",command);
-
-    // START THE LOOP HERE. TERMINATE WHEN WE FIND THAT NEWLINE CHARACTER
-    while (token)
-    {
-        token = strtok_r(NULL, ",", &saveptr);
-        parameters->parameter = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(parameters->parameter, token);
+        printf("%s\n", args[j]);
     }
-    return 0;
-}*/
+}
+
 
 int main()
 {
     
     // Reserve space for a command up to 2048 characters long with two extra for newline
     char userCommand[2050];
+    char *arguments[513];
     char exitCommand[] = "exit\n";
     char expansion[] = "$$";
+    int i = 0;
 
     do
     {
@@ -74,39 +39,28 @@ int main()
     // Read user data
     fgets(userCommand, 2050, stdin);
 
-    // Process command
-    //Remove the newline char to allow fair strcmp.
-    //userCommand[strcspn(userCommand, "\n")] = 0;
+    // Process the command
+    char *ptr;
+    char *token = strtok_r(userCommand, " ", &ptr); 
+    arguments[i] = token;
 
-
-    // If the input is not a newline we process the command.
-    if (strcmp(userCommand, "\n") != 0)
+    if (strcmp(token, "#") != 0)
     {
-        char *saveptr; // used to get a copy of the 
-        char *ptr;
-        // The first token is the command 
-        char *token = strtok_r(userCommand, " ", &ptr);
-        char *command = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(command, token);
-        strcpy(saveptr, ptr);
+        do
+        {   
+            i++;
+            token = strtok_r(NULL, " ", &ptr); 
+            arguments[i] = token;
 
-        token = strtok_r(NULL, "/n", &ptr);
-        char *eof = calloc(strlen(token) + 1, sizeof(char));
-
-        // START HERE, ITERATE THROUGH TO FIND OUT IF THERE'S A NEW LINE.
-
-        // Make sure it's not a comment and see if it has any following words. 
-        if (strcmp(command, "#") != 0)
-        {
-            //processCommand(userCommand, saveptr);
-
-            printf("%d", newCount);
-        }
+        } while (token != NULL && i < 513);
     }
 
-    //execlp(userCommand);
+    for (int j=0; j < i; j++)
+    {
+        printf("%s\n", arguments[j]);
+    }
 
-    } while (strcmp(userCommand, exitCommand) != 0);
+    } while (strcmp(arguments[0], exitCommand) != 0);
 
     // HERE IS WHERE WE'LL CALL/IMPLEMENT THE EXIT HANDLER.
 
